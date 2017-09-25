068635eec7729465291c077cc62caca6f02f65b2
Jhonny Michel <jhonnymichel@evolux.net.br>
2017-09-25 16:31:32 -0300

    INSERTING SCROLLTO LIBRARY

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 require('scss/index.scss');
+
+import ScrollTo from 'storm-scroll-to';
+
+const initialize = () => {
+  ScrollTo.init('.nav__anchor.scroll', {
+    offset: 150,
+    focus: false,
+  });
+}
+
+document.addEventListener('DOMContentLoaded', initialize);
