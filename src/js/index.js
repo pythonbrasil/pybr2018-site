0e3bfe607cb750f6f60d734d07b53a5295db785e
Jhonny Michel <jhonnymichel@evolux.net.br>
2017-09-25 17:58:59 -0300

    adding mobile nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,29 @@
-require('scss/index.scss');
-
+import 'scss/index.scss';
 import ScrollTo from 'storm-scroll-to';
 
-const initialize = () => {
-  ScrollTo.init('.nav__anchor.scroll', {
-    offset: 150,
-    focus: false,
-  });
+class MobileNavManager {
+  constructor() {
+    this.mobileNav = document.querySelector('#mobile-nav');
+    this.mobileNavTrigger = document.querySelector('#mobile-nav-trigger');
+    this.onMobileNavTrigger = this.onMobileNavTrigger.bind(this);
+    this.setupMobileNavigation();
+  }
+
+  setupMobileNavigation() {
+    this.mobileNavTrigger.addEventListener('click', this.onMobileNavTrigger)
+  }
+
+  onMobileNavTrigger(e) {
+    if (this.mobileNav.dataset.opened) {
+      delete this.mobileNav.dataset.opened;
+    } else {
+      this.mobileNav.dataset.opened = true;
+    }
+  }
+
 }
 
-document.addEventListener('DOMContentLoaded', initialize);
+document.addEventListener('DOMContentLoaded', () => {
+  new MobileNavManager();
+  ScrollTo.init('.nav__anchor.scroll');
+});
