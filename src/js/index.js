74cd47ea480bdcdece956148881bd197b94b67a7
Jhonny Michel <jhonnymichel@evolux.net.br>
2017-09-26 09:59:59 -0300

    finishing side nav

--- src/js/index.js
+++ src/js/index.js
@@ -5,19 +5,28 @@ class MobileNavManager {
   constructor() {
     this.mobileNav = document.querySelector('#mobile-nav');
     this.mobileNavTrigger = document.querySelector('#mobile-nav-trigger');
+    this.mobileNavCloseTrigger = document.querySelector('#mobile-nav-close-trigger');
     this.onMobileNavTrigger = this.onMobileNavTrigger.bind(this);
     this.setupMobileNavigation();
   }
 
   setupMobileNavigation() {
-    this.mobileNavTrigger.addEventListener('click', this.onMobileNavTrigger)
+    this.mobileNavTrigger.addEventListener('click', this.onMobileNavTrigger);
+    this.mobileNavCloseTrigger.addEventListener('click', this.onMobileNavTrigger);
+
+    const menuItems = this.mobileNav.querySelectorAll('.nav__anchor');
+    Array.prototype.forEach.call(menuItems, (menuItem) => {
+      menuItem.style.cursor = 'pointer';
+      menuItem.addEventListener('touchend', this.onMobileNavTrigger);
+      menuItem.addEventListener('click', this.onMobileNavTrigger);
+    });
   }
 
   onMobileNavTrigger(e) {
-    if (this.mobileNav.dataset.opened) {
-      delete this.mobileNav.dataset.opened;
-    } else {
-      this.mobileNav.dataset.opened = true;
+    this.mobileNav.classList.toggle('opened');
+    if (e.type == 'touchend') {
+      e.stopPropagation();
+      e.preventDefault();
     }
   }
 
@@ -25,5 +34,8 @@ class MobileNavManager {
 
 document.addEventListener('DOMContentLoaded', () => {
   new MobileNavManager();
-  ScrollTo.init('.nav__anchor.scroll');
+  ScrollTo.init('.nav__anchor.scroll', {
+    offset: 120,
+    focus: false,
+  });
 });
