b149d3873d0d60856fdc3b2be77324dc5fd28819
Fabio Utzig <utzig@apache.org>
2017-11-05 10:00:18 -0200

    Build website to docs

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'bundle.js', 
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   resolve: {
     alias: {
