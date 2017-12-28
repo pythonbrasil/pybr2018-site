65793cc7c62456d72ec32a7ba8e9c0bcf680a39a
Fabio Utzig <utzig@apache.org>
2017-12-28 20:32:56 -0200

    Build output to dist

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'bundle.js', 
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
     alias: {
