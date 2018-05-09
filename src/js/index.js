69df55d504aeba4464476c4e9ce649d9e5b4b28e
Jhonny Michel <jhonnymichel@outlook.com>
2018-05-09 01:30:47 -0300

    adding icon 512

--- src/js/index.js
+++ src/js/index.js
@@ -14,12 +14,13 @@ import Dropdown from 'app/Dropdown';
 import ScrollNavigation from 'scroll-navigation-menu';
 import swURL from 'file-loader?name=sw.js!babel-loader!service-worker';
 import 'file-loader?name=manifest.json!manifest';
-require('icons/icon48.png');
+import 'icons/icon48.png';
 import 'icons/icon72.png';
 import 'icons/icon96.png';
 import 'icons/icon144.png';
 import 'icons/icon168.png';
 import 'icons/icon192.png';
+import 'icons/icon512.png';
 
 const routes = [
   '/codigo-de-conduta',
