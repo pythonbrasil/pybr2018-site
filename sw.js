9820a1798eb5e3e7fbd158a4cfdc900792c7f8e0
Jhonny Michel <jhonnymichel@outlook.com>
2018-06-04 14:03:48 -0300

    rebasing

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-importScripts("/precache-manifest.8989dfd1ee6739f226232d150b2f5805.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+importScripts("/precache-manifest.90d6d959fd7302dc59cd73cbed4465de.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
-const CACHE_VERSION = 'v1';
+const CACHE_VERSION = 'v2';
 const initialCache = [
   '/',
   '/index.html',
