64e5682847b566f7e69ba7d8ad198d3334cc6cb6
Virna Varela <varela.virna@gmail.com>
2018-07-18 14:13:06 -0300

    updating production bundle

--- precache-manifest.2f5ca36de7d8cf1cbd5c5a2bf18eea3e.js
+++ precache-manifest.2f5ca36de7d8cf1cbd5c5a2bf18eea3e.js
@@ -50,7 +50,7 @@ self.__precacheManifest = [
     "url": "/ff274e12ee350a718d77e7e5cb41d4b9.svg"
   },
   {
-    "url": "/66c5c1f941cd0dd1f9f3e9425047efd9.svg"
+    "url": "/4429bbb1fadd88660033c60e25b0d6d0.svg"
   },
   {
     "revision": "f85fbc4dbc08671cc2c1",
@@ -69,7 +69,8 @@ self.__precacheManifest = [
     "url": "/c1e6bce66e35f88b2fdd73d713b8598e.svg"
   },
   {
-    "url": "/4429bbb1fadd88660033c60e25b0d6d0.svg"
+    "revision": "21ab5d3951f82ab30065391e61d446ee",
+    "url": "/Plano_patrocinio_PythonBR_2018_V5.pdf"
   },
   {
     "url": "/96073a188fea734b0408116d8f8eb2e7.jpg"
@@ -78,27 +79,26 @@ self.__precacheManifest = [
     "url": "/bccf71d1f7059651960c22c8acfa48b5.svg"
   },
   {
-    "url": "/ad35fa18a9bb7058faeeedb6d6d2a274.svg"
-  },
-  {
     "url": "/a262af624dbca4dc2228269c1428bff8.svg"
-  },
-  {
-    "url": "/a1cb45781a9958b2542736ff0f094dcd.svg"
-  },
-  {
-    "revision": "21ab5d3951f82ab30065391e61d446ee",
-    "url": "/Plano_patrocinio_PythonBR_2018_V5.pdf"
   },
   {
     "revision": "6f28af5ea8fcf8c0c3e678552076ed5e",
     "url": "/PythonBR14-MidiaKit.zip"
   },
   {
+    "url": "/a1cb45781a9958b2542736ff0f094dcd.svg"
+  },
+  {
+    "url": "/ad35fa18a9bb7058faeeedb6d6d2a274.svg"
+  },
+  {
     "url": "/be9a8528c86f8622188cf6d9186209e5.png"
   },
   {
-    "url": "/8cd9fcdcf4c852651ee31fc81a734378.png"
+    "url": "/19fd2f706aa097db8fadf629de04ac12.svg"
+  },
+  {
+    "url": "/66c5c1f941cd0dd1f9f3e9425047efd9.svg"
   },
   {
     "url": "/8a84d937145140ca758b360009fa410d.svg"
@@ -108,6 +108,9 @@ self.__precacheManifest = [
   },
   {
     "url": "/676242ba1bd587206eb3800c0289d0e0.svg"
+  },
+  {
+    "url": "/8cd9fcdcf4c852651ee31fc81a734378.png"
   },
   {
     "url": "/57aa2a48d7ee76ec1157fdc019230d3a.svg"
@@ -121,9 +124,6 @@ self.__precacheManifest = [
   },
   {
     "url": "/1c14c20d9014206828a1a15ba7f2418b.ico"
-  },
-  {
-    "url": "/19fd2f706aa097db8fadf629de04ac12.svg"
   },
   {
     "url": "/1040749f9476348718b8f6e0e20dc921.jpg"
