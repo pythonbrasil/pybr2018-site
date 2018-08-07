38facd7afc046a401609cffc0cf26c269e1fa08c
Virna Varela <varela.virna@gmail.com>
2018-08-07 09:35:38 -0300

    updating production bundle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.eed569eb5a077f7312aa6927f5cd9ef0.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+importScripts("/precache-manifest.406e8f8174f9ee549136fe0df332f843.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 const CACHE_VERSION = 'v3';
 const initialCache = [
