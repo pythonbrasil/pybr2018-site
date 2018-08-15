3ac6a225fee72a31b569f82bd2503142bb496299
Jhonny Michel <jhonnymichel@evolux.net.br>
2018-08-15 09:57:08 -0300

    filtering data

--- src/js/Schedule.js
+++ src/js/Schedule.js
@@ -1,10 +1,15 @@
 import { CALENDAR_CONFIG } from 'config';
 import React from 'react';
 import get from 'lodash/get';
+import defaultTo from 'lodash/defaultTo';
+import every from 'lodash/every';
+import mapValues from 'lodash/mapValues';
+import map from 'lodash/map';
 import ScrollNavigation from 'scroll-navigation-menu';
 import { StickyContainer, Sticky } from 'react-sticky';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames';
+import Popover from 'react-popover';
 
 function getDayLabel(day){
   switch(day){
@@ -29,6 +34,49 @@ function getFormattedTime(time) {
   return `${hours < 10 ? '0' + hours : hours}h${minutes < 10 ? '0' + minutes : minutes}`;
 }
 
+class FilterBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      popoverAppendTarget: null
+    };
+
+    this.setPopoverAppendTarget = this.setPopoverAppendTarget.bind(this);
+  }
+
+  setPopoverAppendTarget(ref) {
+    this.setState({
+      popoverAppendTarget: document.querySelector('.header__nav')
+    });
+  }
+
+  render() {
+    const { value, onChange, onClick, isPopoverOpened, advancedFilters } = this.props;
+    return (
+      <div className="filter-box" ref={this.setPopoverAppendTarget}>
+        <div className="filters-search">
+          <i className="material-icons">search</i>
+          <input value={value} onChange={onChange} placeholder="Pesquise uma palestra, autor..."/>
+        </div>
+        {this.state.popoverAppendTarget &&
+          <Popover
+            preferPlace="below"
+            refreshIntervalMs={100}
+            enterExitTransitionDurationMs={0}
+            onOuterAction={onClick}
+            isOpen={isPopoverOpened}
+            body={advancedFilters}
+          >
+            <button onClick={onClick} className="filters-button">
+              <i className="material-icons">filter_list</i>
+            </button>
+          </Popover>
+        }
+      </div>
+    );
+  }
+}
+
 const Events = ({ scheduleInDate }) => (
   <article className="schedule_article">
     <h5 className="schedule_time">{getFormattedTime(scheduleInDate.date)}</h5>
@@ -41,10 +89,10 @@ const Events = ({ scheduleInDate }) => (
       <div key={event.id} className={classNames(
         'schedule_info',
         {
-          'col-xl-3 col-lg-6 col-sm-12 schedule-highlight': event.details
+          'col-xl-3 col-lg-6 col-sm-12 schedule-highlight': event.details.eventType !== 'meta'
         }
       )}>
-        {event.details
+        {event.details.eventType !== 'meta'
           ? (
             <React.Fragment>
               <h2 className="schedule_name">{event.summary} <span className="schedule_category"> {event.details.category}</span></h2>
@@ -131,7 +179,7 @@ const CategoryFilter = ({ categories, onChange, filter }) => (
 class Schedule extends React.Component {
   getInitialState(data) {
     const days = {'17': [], '18': [], '22': []};
-    const eventTypes = [];
+    const eventTypes = ['meta'];
     const talksCategories = [];
 
     data.items.forEach(event => {
@@ -148,8 +196,12 @@ class Schedule extends React.Component {
       const pybrEvent = {
         id: event.id,
         date: new Date(startDateTime),
-        summary: event.summary
+        summary: event.summary,
+        details: {
+          eventType: 'meta'
+        }
       }
+
       if (event.description) {
         const [ name, title, eventType, category ] = event.description.split('|');
         pybrEvent.details = {
@@ -203,11 +255,18 @@ class Schedule extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = this.getInitialState(props.data);
+    this.state = {
+      ...this.getInitialState(props.data),
+      searchFilter: '',
+      isShowingAdvancedFilters: false
+    }
     this.onCategoryFilterChange = this.onFilterChange.bind(this, 'categoryFilter');
     this.onTypeFilterChange = this.onFilterChange.bind(this, 'typeFilter');
     this.onClick = this.onClick.bind(this);
     this.filterEvents = this.filterEvents.bind(this);
+    this.toggleAdvancedFilters = this.toggleAdvancedFilters.bind(this);
+    this.onSearchFilterChange = this.onSearchFilterChange.bind(this);
+    this.checkSearchMatch = this.checkSearchMatch.bind(this);
   }
 
   onFilterChange(filterType, filter) {
@@ -225,6 +284,19 @@ class Schedule extends React.Component {
     }
   }
 
+  onSearchFilterChange(e) {
+    this.setState({
+      searchFilter: e.target.value
+    })
+  }
+
+  toggleAdvancedFilters() {
+    this.setState(state => ({
+      ...state,
+      isShowingAdvancedFilters: !state.isShowingAdvancedFilters
+    }))
+  }
+
   componentDidMount() {
     requestAnimationFrame(() => {
       const anchorsOffset = document.querySelector('.filters').getBoundingClientRect().height;
@@ -235,11 +307,22 @@ class Schedule extends React.Component {
     });
   }
 
-  filterEvents(event) {
-    return true;
-    return !event.details
-      || (this.state.typeFilter.includes(event.details.eventType)
-          && (!event.details.category || this.state.categoryFilter.includes(event.details.category)));
+  checkSearchMatch(event) {
+    const searchRegex = new RegExp(this.state.searchFilter.toLowerCase(), 'i');
+    return defaultTo(get(event, 'details.title'), '').match(searchRegex)
+      || defaultTo(get(event, 'details.name'), '').match(searchRegex)
+      || defaultTo(get(event, 'summary'), '').match(searchRegex);
+  }
+
+  filterEvents(acc, { date, events }) {
+    const filteredEvents = events.filter(event => (
+      this.state.typeFilter.includes(event.details.eventType)
+        && (!event.details.category || this.state.categoryFilter.includes(event.details.category))
+        && (!this.state.searchFilter || this.checkSearchMatch(event))
+    ));
+    if (filteredEvents.length)
+      return [ ...acc, { date, events: filteredEvents } ];
+    return acc;
   }
 
   renderDay(day, label) {
@@ -247,7 +330,7 @@ class Schedule extends React.Component {
       return (
         <div id={`day${label}`} >
           <DaySeparator day={label}/>
-          {day.filter(this.filterEvents).map(events => (
+          {day.map(events => (
             <Events scheduleInDate={events} key={getFormattedTime(events.date)} />
           ))}
         </div>
@@ -256,7 +339,9 @@ class Schedule extends React.Component {
  }
 
   render() {
-    const { days, selectedDay, talksCategories, categoryFilter, eventTypes, typeFilter } = this.state;
+    const { days, selectedDay, talksCategories, categoryFilter, eventTypes, typeFilter, searchFilter } = this.state;
+    const filteredDays = mapValues(days, day => day.reduce(this.filterEvents, []));
+    const isListEmpty = every(filteredDays, day => !day.length);
     return (
       <StickyContainer>
         <Sticky>
@@ -266,22 +351,33 @@ class Schedule extends React.Component {
               style={style}
             >
               <DayMenu days={days} selectedDay={selectedDay} onClick={this.onClick}/>
-              {/*<CategoryFilter
-                categories={talksCategories}
-                filter={categoryFilter}
-                onChange={this.onCategoryFilterChange}
+              <FilterBox
+                value={searchFilter}
+                onClick={this.toggleAdvancedFilters}
+                onChange={this.onSearchFilterChange}
+                isPopoverOpened={this.state.isShowingAdvancedFilters}
+                advancedFilters={
+                  <div className="advanced-filters">
+                    <CategoryFilter
+                      categories={talksCategories}
+                      filter={categoryFilter}
+                      onChange={this.onCategoryFilterChange}
+                    />
+                    <EventTypeFilter
+                      types={eventTypes}
+                      filter={typeFilter}
+                      onChange={this.onTypeFilterChange}
+                    />
+                  </div>
+                }
               />
-              <EventTypeFilter
-                types={eventTypes}
-                filter={typeFilter}
-                onChange={this.onTypeFilterChange}
-              />*/}
             </div>
           )}
         </Sticky>
-       {Object.keys(days).map(day => (
-          <React.Fragment key={day}>
-            {this.renderDay(days[day], day)}
+       {isListEmpty && <p className="empty-message">Nenhum evento encontrado.</p>}
+       {map(filteredDays, (day, label) => (
+          <React.Fragment key={label}>
+            {this.renderDay(day, label)}
           </React.Fragment>
         ))}
       </StickyContainer>
