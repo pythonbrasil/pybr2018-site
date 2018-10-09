acfee44b7ff88fd9d00f2b4c71c28e5a802e0d64
Jhonny Michel <jhonnymichel@evolux.net.br>
2018-10-09 17:26:48 -0300

    updating production bundle

--- precache-manifest.04af1c67905259ab6f8a567334dffe93.js
+++ precache-manifest.04af1c67905259ab6f8a567334dffe93.js
@@ -1,16 +1,16 @@
 self.__precacheManifest = [
   {
-    "url": "/./assets/d8bc8ada804c39bd271c7ad46170b546.svg"
+    "url": "/./assets/66c5c1f941cd0dd1f9f3e9425047efd9.svg"
   },
   {
-    "url": "/main.8565b149cc2ca748bc59.js"
+    "url": "/main.e05dbcd194497d08c71d.js"
   },
   {
-    "revision": "82fc3a47c977675971b678f6d3a4990c",
+    "revision": "1672bd04948f10fbaa18f108d943f58a",
     "url": "/sw.js"
   },
   {
-    "url": "/./assets/8b41e927c6b59dafee921aba3e326f76.svg"
+    "url": "/./assets/ad35fa18a9bb7058faeeedb6d6d2a274.svg"
   },
   {
     "revision": "c237550baabb10b69a4664295fe988d4",
@@ -29,7 +29,7 @@ self.__precacheManifest = [
     "url": "/./documents/PythonBR14-MidiaKit.zip"
   },
   {
-    "revision": "41d0490d77e6064951c7",
+    "revision": "795afb63202391444181",
     "url": "/7eafa4f9965ad77aeceabff3493fecc6.css"
   },
   {
@@ -65,49 +65,10 @@ self.__precacheManifest = [
     "url": "/./assets/fe2203988e4d283e51dc0a80b8041b19.svg"
   },
   {
+    "url": "/./assets/d8bc8ada804c39bd271c7ad46170b546.svg"
+  },
+  {
     "url": "/./assets/c1e6bce66e35f88b2fdd73d713b8598e.svg"
-  },
-  {
-    "url": "/./assets/a209d0cd4e6902352a419f276d5dde03.svg"
-  },
-  {
-    "url": "/./assets/bccf71d1f7059651960c22c8acfa48b5.svg"
-  },
-  {
-    "url": "/./assets/a262af624dbca4dc2228269c1428bff8.svg"
-  },
-  {
-    "url": "/./assets/c62e87c2869c12ba8272acc5069c1ad5.svg"
-  },
-  {
-    "url": "/./assets/d63efcfdc56c853dd75c34c995806ed8.svg"
-  },
-  {
-    "url": "/./assets/cace3226fc585b35e358235e2e463b77.svg"
-  },
-  {
-    "url": "/./assets/d6214b39a07aa122734836abf5edd3ae.svg"
-  },
-  {
-    "url": "/./assets/ad35fa18a9bb7058faeeedb6d6d2a274.svg"
-  },
-  {
-    "url": "/./assets/c5162cd0c480a6aabfd03d8eea76e82e.svg"
-  },
-  {
-    "url": "/./assets/be9a8528c86f8622188cf6d9186209e5.png"
-  },
-  {
-    "url": "/./assets/1c14c20d9014206828a1a15ba7f2418b.ico"
-  },
-  {
-    "url": "/./assets/8a84d937145140ca758b360009fa410d.svg"
-  },
-  {
-    "url": "/./assets/a1cb45781a9958b2542736ff0f094dcd.svg"
-  },
-  {
-    "url": "/./assets/66c5c1f941cd0dd1f9f3e9425047efd9.svg"
   },
   {
     "url": "/./assets/676242ba1bd587206eb3800c0289d0e0.svg"
@@ -116,13 +77,52 @@ self.__precacheManifest = [
     "url": "/./assets/69454a80c352c399644073b2d991d6ae.svg"
   },
   {
+    "url": "/./assets/8b41e927c6b59dafee921aba3e326f76.svg"
+  },
+  {
+    "url": "/./assets/bccf71d1f7059651960c22c8acfa48b5.svg"
+  },
+  {
+    "url": "/./assets/a262af624dbca4dc2228269c1428bff8.svg"
+  },
+  {
+    "url": "/./assets/d6214b39a07aa122734836abf5edd3ae.svg"
+  },
+  {
     "url": "/./assets/745113de3d06753dafba58ebc50c5dc4.svg"
   },
   {
-    "url": "/./assets/659d2f554a53d7375c98c1ce2ff95db1.png"
+    "url": "/./assets/d63efcfdc56c853dd75c34c995806ed8.svg"
+  },
+  {
+    "url": "/./assets/8a84d937145140ca758b360009fa410d.svg"
+  },
+  {
+    "url": "/./assets/be9a8528c86f8622188cf6d9186209e5.png"
+  },
+  {
+    "url": "/./assets/cace3226fc585b35e358235e2e463b77.svg"
+  },
+  {
+    "url": "/./assets/c5162cd0c480a6aabfd03d8eea76e82e.svg"
+  },
+  {
+    "url": "/./assets/c62e87c2869c12ba8272acc5069c1ad5.svg"
+  },
+  {
+    "url": "/./assets/a1cb45781a9958b2542736ff0f094dcd.svg"
+  },
+  {
+    "url": "/./assets/a209d0cd4e6902352a419f276d5dde03.svg"
+  },
+  {
+    "url": "/./assets/242d582d169012952f39df817f46b817.png"
   },
   {
     "url": "/./assets/5cda9de029659fb6f704f6d6f6de986d.svg"
+  },
+  {
+    "url": "/./assets/659d2f554a53d7375c98c1ce2ff95db1.png"
   },
   {
     "url": "/./assets/57f237244d6b7ad5a58c9d477b82014e.png"
@@ -134,7 +134,7 @@ self.__precacheManifest = [
     "url": "/./assets/4429bbb1fadd88660033c60e25b0d6d0.svg"
   },
   {
-    "url": "/./assets/242d582d169012952f39df817f46b817.png"
+    "url": "/./assets/1c14c20d9014206828a1a15ba7f2418b.ico"
   },
   {
     "url": "/./assets/19fd2f706aa097db8fadf629de04ac12.svg"
