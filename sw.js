460f84d25786d99d47299cab6a94365e3fe7eff5
Jhonny Michel <jhonnymichel@evolux.net.br>
2018-10-19 04:53:49 -0300

    updating production bundle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.a3f37b9e3ffc6ce7b74adc1048708c76.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+importScripts("/precache-manifest.3d7774fc4bee21d23a33403a9fde0658.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 const CACHE_VERSION = 'v10';
 const initialCache = [
